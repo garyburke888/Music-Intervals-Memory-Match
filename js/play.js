d9f3d30926bb657bd9e64c490174d6bcdb09b37e
garyburke888 <garyburke888@gmail.com>
2020-07-10 09:57:56 +0000

    Added image and audio files, further html & css changes

---
+++ js/play.js
@@ -0,0 +1,6 @@
+// tiles array holds all tiles
+let tile = document.getElementsByClassName("tile");
+let tiles = [...tile];
+
+// collection of all tiles in game
+const collection = document.getElementById("all-tiles");
