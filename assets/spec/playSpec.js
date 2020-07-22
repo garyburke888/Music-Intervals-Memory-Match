6dc9a90aea44ffe2196828ce1bd5e0e308d4828c
garyburke888 <garyburke888@gmail.com>
2020-07-22 10:45:32 +0000

    Initialised Jasmine testing

---
+++ assets/spec/playSpec.js
@@ -0,0 +1,8 @@
+describe("game", function() {
+    describe("start of game", function() {
+        it("should be zero", function() {
+            expect(moveCounter).toBe(0);
+        });
+    });
+
+});
